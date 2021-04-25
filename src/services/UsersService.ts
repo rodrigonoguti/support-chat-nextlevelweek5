ced6dfba99fb7477f282f51d7959b9b15347eda7
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-04-24 23:36:10 -0300

    final lesson

---
+++ src/services/UsersService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+
+    const userAlreadyExists = await this.usersRepository.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      return userAlreadyExists;
+    }
+
+    const user = this.usersRepository.create({ email });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      email
+    });
+
+    return user;
+  }
+}
+
+export { UsersService }
